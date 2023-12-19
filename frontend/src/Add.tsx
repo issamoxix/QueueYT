3c7e78f7612de74666a2b200c95a659e6c49d893
Issam Haidaoui <issamhaidaoui@gmail.com>
2023-12-19 22:00:15 +0100

    minor change to the add page

--- frontend/src/Add.tsx
+++ frontend/src/Add.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react"
 import "./styles/add.css"
 import { addToQueue } from "./components/functions";
+import { Button, TextField } from "@mui/material";
 function Add() {
     const [input, setinput] = useState<string | URL>()
     const [msg, setMsg] = useState<string | null>()
 
     const queryParams = new URLSearchParams(window.location.search);
     const tokenValue = queryParams.get('token');
-    
+
     function onFormSubmit(event: React.FormEvent) {
         event.preventDefault()
-        try{
-            if(input){
+        try {
+            if (input) {
                 const URI = new URL(input)
                 const videoId = URI.searchParams.get("v")
-                if (tokenValue && videoId){
-                    addToQueue(tokenValue, videoId).then((data)=> setMsg(data))
+                if (tokenValue && videoId) {
+                    addToQueue(tokenValue, videoId).then((data) => setMsg(data))
                     setinput("")
                 }
             }
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     }
@@ -29,8 +30,32 @@ function Add() {
             <center>
                 <h1>Add Item</h1>
                 <form onSubmit={onFormSubmit} >
-                    <input value={input instanceof URL ? input.href : input} onChange={(e) => setinput(e.target.value)} type="text" placeholder="Video Url" required />
-                    <input type="submit" value="Add To Queue" />
+                    <TextField sx={{
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                                borderColor: 'white',
+                                color: "white" // Change the border color
+                            },
+                            '&:hover fieldset': {
+                                borderColor: 'blue',
+                                color: "white" // Change the border color on hover
+                            },
+                            '&.Mui-focused fieldset': {
+                                color: "white"
+                            }
+                        }, '&.Mui-focused .MuiInputLabel-outlined': {
+                            color: 'white' // Change the label color when focused
+                        }
+                    }} InputLabelProps={{
+                        style: { color: 'white' }, // changes the label color
+                    }}
+                        inputProps={{
+                            style: { color: 'white' }, // changes the text color
+                        }} id="outlined-basic"
+                        label="Youtube Video "
+                        variant="outlined" value={input instanceof URL ? input.href : input} onChange={(e) => setinput(e.target.value)} />
+                    {/* <input type="submit" value="Add To Queue" /> */}
+                    <Button variant="outlined">Add To Queue</Button>
                 </form>
                 <span>{msg}</span>
             </center>
