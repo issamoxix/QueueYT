ca7bf556c8963c51376587d4d8130acd5782e747
Issam Haidaoui <issamhaidaoui@gmail.com>
2024-05-29 21:03:59 +0200

    change entry point

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,6 +1,6 @@
-import json
+import awsgi
+from .app import app
 
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    return awsgi.response(app, event, context)
